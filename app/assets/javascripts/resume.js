7e19c624f5d2e1ada4a64c8b6bf725aecb4e8bc9
Chris Pereira <chrispereira1@gmail.com>
2016-12-05 00:26:57 -0500

    completely restructure way new page displays, was having trouble with new content flickering

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -3,51 +3,97 @@ $(function(){
   $('header').animate({ opacity: 1 }, 2500);
 
 
-  $('.info-window').animate({ opacity: 1 }, 2500);
+  $('.modal-bodies').animate({ opacity: 1 }, 2500);
 
-
-  $('.about').on('click', function(e) {
+  $('.about').click(function(e) {
     e.preventDefault();
-    $.ajax({
-      url: 'http://localhost:3000/about',
-      method: "GET",
-      dataType: 'HTML',
-    }).done(function(responseData) {
-      console.log(responseData);
-    });
+    var page = $('.modal-body .about');
+    changepage(page);
   });
 
-
-  $('.contact').on('click', function(e) {
+  $('.portfolio').click(function(e) {
     e.preventDefault();
-    $.ajax({
-      url: 'http://localhost:3000/contact',
-      method: "GET",
-      dataType: 'HTML',
-    }).done(function(responseData) {
-      console.log(responseData);
-    });
+    changepage();
+    var page = $(this);
   });
 
-  $('.experience').on('click', function(e) {
+  $('.experience').click(function(e) {
     e.preventDefault();
-    $.ajax({
-      url: 'http://localhost:3000/experience',
-      method: "GET",
-      dataType: 'HTML',
-    }).done(function(responseData) {
-      console.log(responseData);
-    });
+    var page = $('.modal-body .experience');
+    changepage(page);
   });
 
-  $('.portfolio').on('click', function(e) {
+  $('.contact').click(function(e) {
     e.preventDefault();
-    $.ajax({
-      url: 'http://localhost:3000/portfolio',
-      method: "GET",
-      dataType: 'HTML',
-    }).done(function(responseData) {
-      console.log(responseData);
-    });
+    var page = $('.modal-body .contact');
+    changepage(page);
   });
+
+  function changepage(page) {
+    var np = page
+    newpage = np.parents('modal-bodies')
+    currentpage = $('.is-showing')
+
+    currentpage.addClass('animate-out');
+    // animate the page in
+    setTimeout(function(){
+      currentpage.removeClass('animate-out is-showing').next();
+      newpage.addClass('animate-in');
+    }, 600);
+
+    setTimeout(function(){
+      newpage.removeClass('animate-in').addClass('is-showing')
+    }, 1200)
+  }
+  //
+  // function changepage(newpage, currentpage) {
+  //   console.log('new page');
+  //   // animate the page out
+  //               .
+  //
+  // }
+
+  // $('.about').on('click', function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: 'http://localhost:3000/about',
+  //     method: "GET",
+  //     dataType: 'HTML',
+  //   }).done(function(responseData) {
+  //     $('.info-window').html(responseData);
+  //   });
+  // });
+  //
+  // $('.contact').on('click', function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: 'http://localhost:3000/contact',
+  //     method: "GET",
+  //     dataType: 'HTML',
+  //   }).done(function(responseData) {
+  //     $('.info-window').html(responseData);
+  //   });
+  // });
+  //
+  // $('.experience').on('click', function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: 'http://localhost:3000/experience',
+  //     method: "GET",
+  //     dataType: 'HTML',
+  //   }).done(function(responseData) {
+  //     $('.info-window').html(responseData);
+  //   });
+  // });
+  //
+  // $('.portfolio').on('click', function(e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: 'http://localhost:3000/portfolio',
+  //     method: "GET",
+  //     dataType: 'HTML',
+  //   }).done(function(responseData) {
+  //     $('.info-window').html(responseData);
+  //   });
+  // });
 });
