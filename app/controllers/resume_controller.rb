9bc14ea17339f50416b2d45103fdfe132d3cc1a2
Chris Pereira <chrispereira1@gmail.com>
2017-01-12 20:48:07 -0500

    get smtp working

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,8 +1,27 @@
 class ResumeController < ApplicationController
   def new
    @message = Message.new
- end
- 
+  end
+
+  def sendemail
+    Pony.options = {
+      :via => :smtp,
+      :via_options => {
+        address: "smtp.sendgrid.com",
+        port: "587",
+        user_name: "apikey",
+        password: "#{ENV["SG_KEY"]}",
+        :openssl_verify_mode => 'none'
+      }
+    }
+    Pony.mail({
+      from: "#{params["message"][:email]}",
+      to: "chris@cubbie.ca",
+      subject: "test",
+      body: "#{params["message"][:name]} said: #{params["message"][:content]}"
+      })
+  end
+
  def create
    @message = Message.new(message_params)
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'resume#index'
   resources :resume, only: %i(new create)
   post '/', to: 'messages#create'
-
+  post "sendemail" => "resume#sendemail", as: "send_email"
 
 end
